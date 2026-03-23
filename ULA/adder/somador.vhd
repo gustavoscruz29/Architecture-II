735234f3cda85fd4c5f22e6f99ba177797ed1668
gustavoscruz29 <gustavoscruz29@gmail.com>
2026-03-23 15:40:45 -0300

    Update somador.vhd

--- ULA/adder/somador.vhd
+++ ULA/adder/somador.vhd
@@ -10,8 +10,8 @@ architecture arq of somador is
 begin
 	r <= ( (not a) and (not b) and cin ) or
 	     ( (not a) and b and (not cin) ) or
-		   ( a       and b and cin       ) or
-		   ( a       and (not b) and (not cin) );
+		 ( a       and b and cin       ) or
+		 ( a       and (not b) and (not cin) );
 		  
 	cout <= (b and cin) or (a and b) or (a and cin);
 end arq;
