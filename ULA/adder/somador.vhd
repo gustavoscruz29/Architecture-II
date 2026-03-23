11f1c72d2b676a0d161eaf03d3e5e6306e80b428
gustavoscruz29 <gustavoscruz29@gmail.com>
2026-03-23 13:03:40 -0300

    Create somador.vhd

---
+++ ULA/adder/somador.vhd
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity somador is port(
+	a, b, cin: in std_logic;
+	r, cout: out std_logic);
+end somador;
+
+architecture arq of somador is
+begin
+	r <= ( (not a) and (not b) and cin ) or
+	     ( (not a) and b and (not cin) ) or
+		   ( a       and b and cin       ) or
+		   ( a       and (not b) and (not cin) );
+		  
+	cout <= (b and cin) or (a and b) or (a and cin);
+end arq;
