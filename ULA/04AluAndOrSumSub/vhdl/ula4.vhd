6ec1efd2fd2e25bb3191d13364a0c603a368cceb
gustavoscruz29 <gustavoscruz29@gmail.com>
2026-04-09 14:59:18 -0300

    Create ula4.vhd

---
+++ ULA/04AluAndOrSumSub/vhdl/ula4.vhd
@@ -0,0 +1,22 @@
+-- Architecture 4 class 09-04-2026
+-- Gustavo dos Santos Cruz
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity ula4b is port (
+	a, b: in std_logic_vector (3 downto 0); -- like arrays
+	op: in std_logic_vector (2 downto 0);
+	r: out std_logic_vector (3 downto 0)); -- like arrays
+end ula4b;
+
+architecture arq of ula4b is
+	signal fio1,fio2,fio3,fio4: std_logic;
+begin
+	-- maps
+	-- Currently, the adder only performs sums
+	u0: work.celula1b port map (a=>a(0), b=>b(0), cin=>op(2), negaB=>op(2), cout=>fio1, r=>r(0), op=>op(1 downto 0));
+	u1: work.celula1b port map (a=>a(1), b=>b(1), cin=>fio1,  negaB=>op(2), cout=>fio2, r=>r(1), op=>op(1 downto 0));
+	u2: work.celula1b port map (a=>a(2), b=>b(2), cin=>fio2,  negaB=>op(2), cout=>fio3, r=>r(2), op=>op(1 downto 0));
+	u3: work.celula1b port map (a=>a(3), b=>b(3), cin=>fio3,  negaB=>op(2), cout=>fio4, r=>r(3), op=>op(1 downto 0));
+end arq;
